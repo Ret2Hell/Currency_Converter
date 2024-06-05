6160cd37c6737cf82d48675f642eed5d14eb9e5f
tayebyassine <tayeb1920@gmail.com>
2024-06-05 11:03:50 +0100

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:currency_converter/currency_converter.dart';
+import 'package:currency_converter/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CurrencyConverter(),
+    return MaterialApp(
+      title: 'Currency Converter',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => const CurrencyConverter(),
+      },
     );
   }
 }
---
+++ lib/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/icon.png', height: 150, width: 150),
+              ],
+            ),
+          ),
+          const Spacer(),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text(
+              'By Ret2Hell',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
